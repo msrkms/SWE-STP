757582af907a49573bfe937c2c8612dd4f7c57bf
SHIKHON <ahsanul35-1838@diu.edu.bd>
2019-11-19 23:47:58 +0600

    event add activity added

---
+++ app/src/main/java/com/sajidur/swe_stp/EventAddActivity.java
@@ -0,0 +1,14 @@
+package com.sajidur.swe_stp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_add);
+    }
+}
