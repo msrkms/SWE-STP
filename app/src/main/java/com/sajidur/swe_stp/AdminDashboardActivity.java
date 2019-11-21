f0ea68267d55392312cbe2b8bb60bf7b5e28d929
msrkms <sajidur35-1831@diu.edu.bd>
2019-11-21 15:22:11 +0600

    marge conflict between branch solved

--- app/src/main/java/com/sajidur/swe_stp/AdminDashboardActivity.java
+++ app/src/main/java/com/sajidur/swe_stp/AdminDashboardActivity.java
@@ -2,13 +2,66 @@ package com.sajidur.swe_stp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.card.MaterialCardView;
 
 public class AdminDashboardActivity extends AppCompatActivity {
+    private MaterialCardView materialCardViewClassRoutine,materialCardViewExamRoutine,materialCardViewEvents,materialCardViewCourseOffer,materialCardViewLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dashboard);
+
+        materialCardViewClassRoutine=(MaterialCardView) findViewById(R.id.materialCardViewClassRoutine);
+        materialCardViewCourseOffer=(MaterialCardView) findViewById(R.id.materialCardViewCourseOffer);
+        materialCardViewEvents=(MaterialCardView) findViewById(R.id.materialCardViewEvents);
+        materialCardViewExamRoutine=(MaterialCardView) findViewById(R.id.materialCardViewExamRoutine);
+        materialCardViewLogout=(MaterialCardView) findViewById(R.id.materialCardViewLogout);
+
+        materialCardViewEvents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AdminDashboardActivity.this,EventListActivity.class));
+            }
+        });
+
+
+        materialCardViewExamRoutine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               // startActivity(new Intent(AdminDashboardActivity.this,UploadRoutineActivity.class));
+               // AdminDashboardActivity.this.finish();
+            }
+        });
+
+
+        materialCardViewClassRoutine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AdminDashboardActivity.this,UploadRoutineActivity.class));
+            }
+        });
+
+
+        materialCardViewCourseOffer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AdminDashboardActivity.this,UploadCourseOfferActivity.class));
+            }
+        });
+
+
+        materialCardViewLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AdminDashboardActivity.this.finish();
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/sajidur/swe_stp/EventListActivity.java
+++ app/src/main/java/com/sajidur/swe_stp/EventListActivity.java
@@ -2,13 +2,32 @@ package com.sajidur.swe_stp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class EventListActivity extends AppCompatActivity {
+
+
+    private MaterialButton materialButtonAddNewEvent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_list);
+
+        materialButtonAddNewEvent=(MaterialButton) findViewById(R.id.materialButtonAddNewEvent);
+
+
+        materialButtonAddNewEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(EventListActivity.this,EventAddActivity.class));
+                finish();
+            }
+        });
+
     }
 }
