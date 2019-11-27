422404f1e849f8b963cd432df2c3dc128ee50682
msrkms <sajidur35-1831@diu.edu.bd>
2019-11-28 01:49:44 +0600

    Profile,ClassRoutineAdeed

---
+++ app/src/main/java/com/sajidur/swe_stp/ChoseClassRoutineTypeActivity.java
@@ -0,0 +1,14 @@
+package com.sajidur.swe_stp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChoseClassRoutineTypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chose_class_routine_type);
+    }
+}
--- app/src/main/java/com/sajidur/swe_stp/StudentDashboardActivity.java
+++ app/src/main/java/com/sajidur/swe_stp/StudentDashboardActivity.java
@@ -3,12 +3,25 @@ package com.sajidur.swe_stp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class StudentDashboardActivity extends AppCompatActivity {
+    private TextView textViewClassRoutine,textViewExamRoutine,textViewAppointment,textViewClassDiscussion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_dashboard);
+
+        textViewAppointment=(TextView) findViewById(R.id.textViewAppointment);
+        textViewClassDiscussion=(TextView) findViewById(R.id.textViewClassDiscussion);
+        textViewClassRoutine=(TextView) findViewById(R.id.textViewClassRoutine);
+        textViewExamRoutine=(TextView) findViewById(R.id.textViewExamRoutine);
+
+
+        textViewExamRoutine.setSelected(true);
+        textViewClassRoutine.setSelected(true);
+        textViewClassDiscussion.setSelected(true);
+        textViewAppointment.setSelected(true);
     }
 }
